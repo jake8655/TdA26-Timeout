4f0a2e6f799029ace40f2e52c390130466663c73
Jake <dominik8655@gmail.com>
2025-12-14 17:01:54 +0100

    chore: setup font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+const fontSans = localFont({
+	src: "./dosis.ttf",
+	variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +19,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-				{children}
-			</body>
+		<html lang="en" className={fontSans.variable}>
+			<body className="antialiased">{children}</body>
 		</html>
 	);
 }

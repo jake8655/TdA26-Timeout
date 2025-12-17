b2657c040600f75bca72ebbe07e5f4f69f53cfad
hypnomacka <sovoman7@gmail.com>
2025-12-17 10:22:25 +0100

    chore: initialize Spring Boot server with Gradle setup and basic configuration

---
+++ apps/server/src/main/java/eu/hypnomacka/timeout/server/controllers/RootController.java
@@ -0,0 +1,16 @@
+package eu.hypnomacka.timeout.server.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public Map<String, String> root() {
+        return Map.of("organization", "Student Cyber Games");
+    }
+}

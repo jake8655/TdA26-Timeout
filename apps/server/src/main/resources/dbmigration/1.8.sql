23ef6c14d2862d2c9b988b9121d378a998590aa3
Jake <dominik8655@gmail.com>
2026-02-19 15:55:42 +0100

    chore: migrate
    Fuck migrations, fuck java, fuck ebean

--- apps/server/src/main/resources/dbmigration/1.8.sql
+++
@@ -1,6 +0,0 @@
--- apply changes
-alter table questions add column position integer not null default 0;
-
--- index for efficient ordering
-create index ix_questions_quiz_position on questions (quiz_uuid, position);
-
